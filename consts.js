2c183961b8ef572599cede380a5a7330b9d6b883
david.b <david.b@liquidapps.io>
2019-06-11 14:38:47 +0300

    yalla

---
+++ consts.js
@@ -0,0 +1,15 @@
+
+const MNEMONIC = 'series guide rather talk giant return police sick dune wear deny quantum';
+
+const PRIVATE_KEY = '0xb4ffe5c33110be659ecfb19d2a6088c686f91103dad29e220ad762f9e6008654';
+
+const ACCOUNT_ADDRESS = '0xeb9303cab91abe9667f9e8716363ab18a02b107e';
+
+const WEB3_PROVIDER = process.env.WEB3_PROVIDER || 'http://localhost:8545';
+
+module.exports = {
+  MNEMONIC,
+  PRIVATE_KEY,
+  ACCOUNT_ADDRESS,
+  WEB3_PROVIDER
+}
--- scripts/migration.js
+++ scripts/migration.js
@@ -1,9 +1,24 @@
-const web3 = require('Web3');
+const Web3 = require('web3');
+const fs = require('fs');
+const consts = require('../consts'); // say it three times
 
-const WEB3_PROVIDER = process.env.WEB3_PROVIDER || '127.0.0.1:8545';
+const daiBytecode = fs.readFileSync(`${__dirname}/../build/Dai.bin`);
+const daiAbi = JSON.parse(fs.readFileSync(`${__dirname}/../build/Dai.abi`));
 
 async function main() {
-
+  let web3 = new Web3(consts.WEB3_PROVIDER);
+  let accounts = await web3.eth.getAccounts();
+  let daiContract = new web3.eth.Contract(daiAbi);
+  let dai = await daiContract.deploy({ data: daiBytecode }).send({
+    from: accounts[0],
+    gas: '3000000'
+  });
+  fs.writeFileSync(
+    `${__dirname}/../addresses.json`,
+    JSON.stringify({ address: dai.options.address }),
+    'utf-8'
+  );
 }
+
 
 main().catch(console.log);
